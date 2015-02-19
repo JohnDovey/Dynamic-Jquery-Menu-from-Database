af768219791eda13446bbb30715aaac23062b71e
John Dovey <dovey.john@gmail.com>
2015-02-19 11:16:27 +0200

    Created dbconnect.php

---
+++ DynMenu/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright John Dovey, 2010
+// john@justdone.co.za
+// Ver 1.1
+//
+mysql_connect("mysql-d", "d309002rw","dynmenu") or
+	die ("Could not connect to database");
+mysql_select_db("d309002_menu") or
+	die ("Could not select database");
+?>
