cab4421f18e660c7214dd1db2aaf953ca6ac0310
John Dovey <dovey.john@gmail.com>
2020-12-26 12:12:16 -0600

    Update index.php
    -- Updated code formatting

--- DynMenu/index.php
+++ DynMenu/index.php
@@ -11,9 +11,9 @@ include_once ("dbconnect.php"); // Connect to the Database
     <title>Menu Demo</title>
     <link rel="stylesheet" type="text/css" href="jquery/jqueryslidemenu.css" />
     <!-- Note: The old version of Jquery is included in the project, but in this file show the better practice of including version 3.5.1 version from CDN. If this dowsn't work for you, try uncommenting the line below for version 2.1.3 -->
-   <!-- <script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.min.js"></script> -->
-   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
+    <!-- <script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="jquery/jqueryslidemenu.js"></script>
     <link rel="stylesheet" type="text/css" href="default.css">
 </head>
