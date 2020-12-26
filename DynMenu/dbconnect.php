f98c738658ced3fdd90fbfa2c4fb803e504f28cd
John Dovey <dovey.john@gmail.com>
2020-12-26 11:40:29 -0600

    Update dbconnect.php
    -- Changed password
    -- Updated version count
    -- Updated parameters of DB Connection to be generic
    -- Added "official" definition for database server connection 
    -- Added "official" definition for database selection

--- DynMenu/dbconnect.php
+++ DynMenu/dbconnect.php
@@ -1,10 +1,15 @@
 <?php
 // Copyright John Dovey, 2010
-// john@justdone.co.za
-// Ver 1.1
+// dovey.john@gmail.com
+// Ver 1.2
 //
-mysql_connect("mysql-d", "d309002rw","dynmenu") or
+// mysql_connect ([ string $server = ini_get("mysql.default_host") [, string $username = ini_get("mysql.default_user") [, string $password = ini_get("mysql.default_password") [, bool $new_link = false [, int $client_flags = 0 ]]]]] ) : resource|false
+//
+mysql_connect("MyHost", "MyUsername","MyPassword") or
 	die ("Could not connect to database");
-mysql_select_db("d309002_menu") or
+//
+//mysql_select_db ( string $database_name [, resource $link_identifier = NULL ] ) : bool
+//
+mysql_select_db("MyMenuDB") or
 	die ("Could not select database");
 ?>
